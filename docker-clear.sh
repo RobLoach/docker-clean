c55bb722234c65326b4d8b2a020c0f6878e5c376
Rob Loach <robloach@gmail.com>
2015-01-21 02:06:11 -0500

    Add the volumes key to docker rm

--- docker-clear.sh
+++ docker-clear.sh
@@ -4,7 +4,7 @@
 docker stop --time=5 $(docker ps -a -q)
 
 # Remove all the container images
-docker rm --force=true $(docker ps -a -q)
+docker rm --force=true --volumes=true $(docker ps -a -q)
 
 # Clear all saved images
 docker rmi --force=true $(docker images -a -q)
